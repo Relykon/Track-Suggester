37f3188dcffd39bdede93e25fb84dcc9a7dbf4a0
Kyle Harrington <harrington.kyle@gmail.com>
2019-04-02 20:16:50 -0700

    add improved results content, styling, and structure. add working submit, and improved back-end logic

---
+++ README.md
@@ -0,0 +1,32 @@
+# Track_Suggester
+
+#### A portfolio of my work, 03.22.2019
+
+#### By **Kyle Harrington**
+
+## Description
+
+This page is a survey that's intent is to help a person interested in programming narrow down their preferred choice of languages.
+
+## Setup/Installation Requirements
+
+* clone the GitHub repository: https://github.com/Relykon/Portfolio.git
+* open with the text editor of your choice
+* or view live at GitHub: https://relykon.github.io/Portfolio/  
+
+
+## Known Bugs
+
+No known issues
+
+## Support and contact details
+
+Any questions or suggestions? _Contact Kyle_ at <a href = "mailto: harrington.kyle@gmail.com">harrington.kyle@gmail.com</a>
+
+## Technologies Used
+
+HTML, CSS, Bootstrap, jQuery
+
+### Legal
+
+Copyright (c) 2019 **By Kyle Harrington**
--- js/scripts.js
+++ js/scripts.js
@@ -1,34 +1,36 @@
 $(document).ready(function() {
-  $("#btn1").click(function() {
+  $("#showQuiz").click(function() {
     $("#quiz").slideToggle();
   });
 
   $("#quiz").submit(function(event) {
+    event.preventDefault();
+
     var Q1 = parseInt($("input:radio[name=Q1]:checked").val());
     var Q2 = parseInt($("input:radio[name=Q2]:checked").val());
     var Q3 = parseInt($("input:radio[name=Q3]:checked").val());
-    var Q4 = paresInt($("input:radio[name=Q4]:checked").val());
-    var Q5 = paresInt($("input:radio[name=Q5]:checked").val());
+    var Q4 = parseInt($("input:radio[name=Q4]:checked").val());
+    var Q5 = parseInt($("input:radio[name=Q5]:checked").val());
 
-    var language = Q1+Q2+Q3+Q4+Q5;
+
+    var language = (Q1+Q2+Q3+Q4+Q5);
+
+    $("#results").show();
+    $("#quiz").slideToggle();
+    $("#showQuiz").text("Try Again");
 
     if (language < 7) {
       $("#result2").hide();
       $("#result3").hide();
       $("#result1").show();
-    }
-    else if (language = 7 || 8) {
+    } else if (language == 7 || language == 8) {
       $("#result1").hide();
       $("#result3").hide();
-      $("#chris").show();
-    }
-
-    else {
+      $("#result2").show();
+    } else {
       $("#result1").hide();
       $("#result2").hide();
       $("result3").show();
     }
-    
-    event.preventDefault();
   });
 });
